a7eb505213f85c38746831c05ebb596e46e6bb83
Sriram Damodharan <sdamodharan@thapovan-inc.com>
2018-06-19 15:00:22 +0530

    Day 1 solution

--- Day1.go
+++ Day1.go
@@ -1,9 +1,15 @@
 package main
 
 func HelloWorld() string {
-	panic("Yet to be implemented")
+	return "Hello World"
 }
 
 func Fib(num,a,b uint64) uint64  {
-	panic("Yet to be implemented")
+	if num == 0 {
+		return a
+	} else if num == 1 {
+		return b
+	} else {
+		return Fib(num-1,b,a+b)
+	}
 }
